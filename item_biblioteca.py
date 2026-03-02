393ff386f73fc15248628e02d4dfb41ea5e6913e
Felipe <felipecezaralmeida0@gmail.com>
2026-03-02 11:28:18 -0300

    2. cria classe ItemBiblioteca com encapsulamento

---
+++ item_biblioteca.py
@@ -0,0 +1,7 @@
+class ItemBiblioteca:
+    def __init__ (self,codigo:str, titulo:str, ano:int, disponivel:bool):
+        self.__codigo = codigo
+        self.__titulo = titulo
+        self.__ano = ano
+        self.__disponivel = disponivel
+
