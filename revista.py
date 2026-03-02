f3b285d57e5a7e9a568a52fb359138d30fdbb148
Felipe <felipecezaralmeida0@gmail.com>
2026-03-02 13:05:39 -0300

    5. cria classe Revista herdando Item Biblioteca

--- revista.py
+++ revista.py
@@ -1,1 +1,18 @@
+import item_biblioteca
 
+class Revista(item_biblioteca.ItemBiblioteca):
+    def __init__(self, codigo: str, titulo: str, ano: int, edicao: str, mes: str):
+        super().__init__(codigo, titulo, ano, True)
+        self.__edicao = edicao
+        self.__mes = mes
+
+    def exibir_detalhes(self):
+        super().exibir_detalhes()
+        print(f"Edição: {self.__edicao}")
+        print(f"Mês: {self.__mes}")
+
+    def get_edicao(self):
+        return self.__edicao
+
+    def get_mes(self):
+        return self.__mes
