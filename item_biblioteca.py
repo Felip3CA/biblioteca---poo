9d3d1cdd087253cf9f2849e54e63181b83c96f34
Felipe <felipecezaralmeida0@gmail.com>
2026-03-02 12:41:35 -0300

    3. adiciona emprestar e devolver no ItemBiblioteca

--- item_biblioteca.py
+++ item_biblioteca.py
@@ -5,3 +5,38 @@ class ItemBiblioteca:
         self.__ano = ano
         self.__disponivel = disponivel
 
+    def exibir_detalhes(self):
+        print(f"Código: {self.__codigo}")
+        print(f"Título: {self.__titulo}")
+        print(f"Ano: {self.__ano}")
+
+    def emprestar(self):
+        if self.__disponivel == False:
+            print("O item não está disponível para empréstimo.")
+        else:
+            self.__disponivel = False
+            print("Empréstimo realizado com sucesso.")
+
+    def devolver(self):
+        if self.__disponivel == True:
+            print("O item já está disponível na biblioteca.")
+        else:
+            self.__disponivel = True
+            print("Devolução realizada com sucesso.")
+
+    def set_validacao(self, codigo:str, titulo:str, ano:int):
+        if len(codigo) == 0:
+            print("Código inválido. O código não pode ser vazio.")
+        else:
+            self.__codigo = codigo
+        if len(titulo) == 0:
+            print("Título inválido. O título não pode ser vazio.")
+        else:
+            self.__titulo = titulo
+        if ano < 0:
+            print("Ano inválido. O ano não pode ser negativo.")
+        else:
+            self.__ano = ano
+            
+    def get_codigo(self):
+        return self.__codigo
